cbb1e9a568c8e26c2943bcc9df1990853dbb9275
graef <simonjn45@gmail.com>
2024-10-30 18:19:55 +0100

    chore: more boilerplate

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/anthonynsimon/bild/imgio"
 	"github.com/anthonynsimon/bild/transform"
@@ -11,6 +12,8 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/valyala/fasttemplate"
 )
+
+// The Commands
 
 var (
 	rootCmd = &cobra.Command{
@@ -30,27 +33,13 @@ var (
 	initCmd = &cobra.Command{
 		Use:   "init",
 		Short: "Initialize a project",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Init...")
-		},
+		Run:   initFilm,
 	}
 
 	addCmd = &cobra.Command{
 		Use:   "add",
 		Short: "add a picture",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Add picture...")
-			ds, err := disk.New("./film", ".lock")
-			if err != nil {
-				fmt.Println(err)
-			}
-			db, err := hare.New(ds)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			db.Close()
-		},
+		Run:   addPicture,
 	}
 
 	removeCommand = &cobra.Command{
@@ -72,28 +61,87 @@ var (
 	}
 )
 
-func build(cmd *cobra.Command, args []string) {
-	fmt.Println("Build...")
-	img, err := imgio.Open("input.jpg")
+// Picture Helpers
+
+type PictureType struct {
+	OTHER int
+	JPEG  int
+	PNG   int
+}
+
+var PType PictureType = PictureType{
+	OTHER: 0,
+	JPEG:  1,
+	PNG:   2,
+}
+
+// DB Helpers
+
+func openDB() *hare.Database {
+	ds, err := disk.New("./film", ".json")
+	if err != nil {
+		fmt.Println("Could not finde the 'film.json' file.\nDid you use 'film init'?")
+		os.Exit(1)
+	}
+	db, err := hare.New(ds)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return db
+
+}
+
+// Start of commands
+
+func initFilm(cmd *cobra.Command, args []string) {
+	fmt.Println("Init...")
+}
+
+func addPicture(cmd *cobra.Command, args []string) {
+	db := openDB()
+
+	db.Close()
+}
+
+func buildPictureSite(picture string, title string, id int) {
+	// opening the image and getting the bounds to use them later
+	img, err := imgio.Open(picture)
+	fmt.Println(img.Bounds().String())
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	resized := transform.Resize(img, 800, 800, transform.Linear)
-	if err := imgio.Save("output.png", resized, imgio.PNGEncoder()); err != nil {
+
+	// refactor into an own function that can *dynamically*
+	// resize the picture to make it way more resiliant
+	resized := transform.Resize(img, 400, 400, transform.Linear)
+	if err := imgio.Save(strconv.Itoa(id)+".jpg", resized, imgio.JPEGEncoder(100)); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	template := "<h1>{{ string }}</h1>"
+	template := "<html><h1>{{title}}</h1></html>"
 	t := fasttemplate.New(template, "{{", "}}")
 	s := t.ExecuteString(map[string]interface{}{
-		"string": "Hello, World!",
+		"title": title,
 	})
 	fmt.Println(s)
 }
 
+func buildArchiveSite() {
+
+}
+
+func build(cmd *cobra.Command, args []string) {
+	db := openDB()
+	fmt.Println("Build...")
+	buildPictureSite("picture.jpg", "", 1)
+	buildArchiveSite()
+	db.Close()
+}
+
 func init() {
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	rootCmd.AddCommand(versionCmd, initCmd, addCmd, buildCmd, removeCommand, listCommand)
 }
 
